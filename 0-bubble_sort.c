fb07e00d32f8978589870220422fa8fcc6bc22f5
IgnacioCabrera18 <IgnacioCabrera1832006@gmail.com>
2025-08-06 15:21:53 +0000

    culpa de cape

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size; i++)
+			if (i < size && array[i] > array[i + 1])
+			{
+				temp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+			}
+	}
+}
